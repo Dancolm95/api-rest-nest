dd4e11f12f5e696e4940cbecb792bb3b20bc06e7
Daniel Colona <dancolm95@gmail.com>
2025-04-29 11:41:29 -0500

    fix the structure of the project

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Get, Post, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { TasksService } from './tasks/tasks.service';
 
 @Controller()
 export class AppController {
@@ -9,4 +10,16 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Post()
+  createTask(@Body() body: { description: string }) {
+    const description = body.description;
+
+    if (!description.trim() || description.trim().length === 0)
+      throw new BadRequestException('La tarea no es válida ya que esta vacía');
+    else {
+      const newTask = this.tasksService.createTask(description);
+      const updateTask = this.taskService.findAll();
+      }
+    }
 }
